92880f69f0f2d09d6b1eaa067c045ebd88a31d24
shashwat172002 <shashwatmani17@gmail.com>
2024-09-15 11:45:50 +0530

    fc-9

--- api/index.js
+++ api/index.js
@@ -38,7 +38,7 @@ const io = new Server(server, {
 
 app.use(cors(
   {
-    origin: "https://dorm-drop-backend.vercel.app/",       //https://dormdrop.onrender.com  //http://localhost:5173
+    origin: "https://dormdrop.vercel.app/",       //https://dormdrop.onrender.com  //http://localhost:5173
   }
 ))
 
